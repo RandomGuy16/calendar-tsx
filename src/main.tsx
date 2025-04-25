0f45a42da983c074d7b301a384b5027b091b3b51
RandomUser16 <aroon.delgado.25@gmail.com>
2025-04-24 20:34:23 -0500

    Succesful sass modules translation
    the styles are not too precise since naming conventions but can be fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router"
-import './styles/index.scss'
+import './global/index.scss'
 import App from './App.tsx'
 import data from './assets/data.json'
 
