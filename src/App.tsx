786910819b6f807a7c1cad8533ffca30d38d0501
RandomGuy16 <aroon.delgado.25@gmail.com>
2025-07-18 20:49:58 -0500

    Chore: Install Tailwind CSS for transitioning

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import styles from './global/App.module.scss'
 
 function App() {
 
-    return (
-        <>
-            <header className={styles.App_header}>
-                <Header />
-            </header>
-            <main className={styles.App_main}>
-                <SchedulePlanner></SchedulePlanner>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <header className="h-5 w-full bg-red-500">
+        <Header />
+        <span className="bg-red-500">hola</span>
+      </header>
+      <main className={styles.App_main}>
+        <SchedulePlanner></SchedulePlanner>
+      </main>
+    </>
+  )
 }
 
 export default App
